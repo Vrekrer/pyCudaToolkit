d86bafc096fa6f9fcbcaab38f628c34175a82f05
Vrekrer <diegogch@cbpf.br>
2015-06-16 18:08:40 -0300

    Hello Version!
    Library import
    cuBLAS Datatypes defined
    cublasCreate, cublasDestroy and cublasGetVersion wrappers added
    Test functions working!

--- pucudablas.py
+++
@@ -1,8 +0,0 @@
-import ctypes, platform
-
-#  cuBLAS Library 
-
-if platform.system()=='Microsoft': libcublas = ctypes.windll.LoadLibrary('cublas.dll')
-if platform.system()=='Linux':     libcublas = ctypes.cdll.LoadLibrary('libcublas.so')
-else:                              libcublas = ctypes.cdll.LoadLibrary('libcublas.so')
-
---
+++ pycublas.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+"""
+Raw ctypes wrappers of the cuBLAS library (v7.0)
+For documentation see:
+http://docs.nvidia.com/cuda/cublas/index.htm
+cublas_api.h and cublas_v2.h
+"""
+
+import platform, ctypes, ctypes.util, enum
+from ctypes import *
+
+#cuBLAS Library 
+libname = ctypes.util.find_library('cublas')
+if platform.system()=='Microsoft': 
+    libcublas = ctypes.windll.LoadLibrary(libname)
+elif platform.system()=='Linux':     
+    libcublas = ctypes.CDLL(libname, ctypes.RTLD_GLOBAL)
+else:
+    libcublas = ctypes.cdll.LoadLibrary(libname)
+
+## cuBLAS Datatypes ##
+
+#cublasStatus_t
+class cublasStatus_t(enum.IntEnum):
+    CUBLAS_STATUS_SUCCESS         =0
+    CUBLAS_STATUS_NOT_INITIALIZED =1
+    CUBLAS_STATUS_ALLOC_FAILED    =3
+    CUBLAS_STATUS_INVALID_VALUE   =7
+    CUBLAS_STATUS_ARCH_MISMATCH   =8
+    CUBLAS_STATUS_MAPPING_ERROR   =11
+    CUBLAS_STATUS_EXECUTION_FAILED=13
+    CUBLAS_STATUS_INTERNAL_ERROR  =14
+    CUBLAS_STATUS_NOT_SUPPORTED   =15
+
+#cublasFillMode_t
+class cublasFillMode_t(enum.IntEnum):
+    CUBLAS_FILL_MODE_LOWER=0 
+    CUBLAS_FILL_MODE_UPPER=1
+
+#cublasDiagType_t
+class cublasDiagType_t(enum.IntEnum):
+    CUBLAS_DIAG_NON_UNIT=0
+    CUBLAS_DIAG_UNIT=1
+
+#cublasSideMode_t
+class cublasSideMode_t(enum.IntEnum):
+    CUBLAS_SIDE_LEFT =0 
+    CUBLAS_SIDE_RIGHT=1
+
+#cublasOperation_t
+class cublasOperation_t(enum.IntEnum):
+    CUBLAS_OP_N=0
+    CUBLAS_OP_T=1
+    CUBLAS_OP_C=2
+
+#cublasPointerMode_t
+class cublasPointerMode_t(enum.IntEnum):
+    CUBLAS_POINTER_MODE_HOST   = 0
+    CUBLAS_POINTER_MODE_DEVICE = 1
+
+#cublasAtomicsMode_t
+class cublasAtomicsMode_t(enum.IntEnum):
+    CUBLAS_ATOMICS_NOT_ALLOWED   = 0
+    CUBLAS_ATOMICS_ALLOWED       = 1
+
+#/* Opaque structure holding CUBLAS library context */
+# struct cublasContext;
+# typedef struct cublasContext *cublasHandle_t;
+class _opaque(ctypes.Structure):
+    pass
+cublasHandle_t = POINTER(_opaque)
+cublasHandle_t.__name__ = 'cublasHandle_t'
+
+##cuBLAS Helper Functions##
+
+# cublasStatus_t
+# cublasCreate(cublasHandle_t *handle)
+cublasCreate = libcublas.cublasCreate_v2
+cublasCreate.restype = cublasStatus_t
+cublasCreate.argtypes = [POINTER(cublasHandle_t)]
+
+# cublasStatus_t
+# cublasDestroy(cublasHandle_t handle)
+cublasDestroy = libcublas.cublasDestroy_v2
+cublasDestroy.restype = cublasStatus_t
+cublasDestroy.argtypes = [cublasHandle_t]
+
+# cublasStatus_t
+# cublasGetVersion(cublasHandle_t handle, int *version)
+cublasGetVersion = libcublas.cublasGetVersion_v2
+cublasGetVersion.restype = cublasStatus_t
+cublasGetVersion.argtypes = [cublasHandle_t, POINTER(c_int)]
+
+#Test functions
+handle = cublasHandle_t()
+def Init():
+  status = cublasCreate(byref(handle))
+  print status
+
+def Close():
+  status = cublasDestroy(handle)
+  print status
+
+def Version():
+  version = c_int()
+  status = cublasGetVersion(handle, byref(version))
+  print 'version:', version.value
+  print status
